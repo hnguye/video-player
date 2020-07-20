e172b0d9ef47bf2cf269f3d4f8d44d95f073837a
hnguye <hieutnguyen06@gmail.com>
2020-07-20 21:19:40 +1000

    Reformat code

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const toggle = player.querySelector(".toggle");
 const skipButtons = player.querySelectorAll("[data-skip]");
 const ranges = player.querySelectorAll("player__slider");
 
-// Build our functions
+// Media functions
 function togglePlay() {
 	const method = video.paused ? "play" : "pause";
 	video[method]();
@@ -36,7 +36,7 @@ function scrub(e) {
 	const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
 	video.currentTime = scrubTime;
 }
-// Hook up the evenet listener
+// Listen to DOM events
 video.addEventListener("click", togglePlay);
 video.addEventListener("play", updateButton);
 video.addEventListener("paused", updateButton);
